b7c515181b1c5cf11bca127edb1fe7ae2c07df10
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-02-21 14:44:36 +0300

    feat: add Questions component

---
+++ src/pages/savanna/Questions.tsx
@@ -0,0 +1,70 @@
+import { QuestionAnswer } from "@mui/icons-material";
+import React, {useState, useEffect} from "react";
+import { Word } from "types";
+import style from './Questions.scss';
+
+const url = 'https://rslang-team54.herokuapp.com';
+
+export const Questions = ({words, answer, handleAnswer, handleWrongAnswer, isAnswered, nextRound}:  
+  {words: Word[], answer: Word | null, handleAnswer: (wordId: string) => void,
+    handleWrongAnswer: () => void,
+    isAnswered: boolean, nextRound: () => void}) => {
+  
+
+  const [selected, setSelected] = useState('');
+
+  const checkAnswer = (event: any) => {
+   // if (!isAnswered) {
+      //
+
+      handleAnswer(event.target.value);
+      setSelected(event.target.value);
+      setTimeout(() => nextRound(), 1000);
+   // }
+
+  }
+
+
+  useEffect(() => {
+    //let timer: any;
+     const timer = setInterval(() => {
+       if (answer) {
+        handleWrongAnswer();
+        setTimeout(() => nextRound(), 1000);
+       }
+      
+     }, 4000);
+    // if (answer) {
+    //   //playAudio();
+    //   timer = setTimeout(() => {
+    //     nextRound();
+    //   }, 5000);
+    // }
+    return () => {
+      clearInterval(timer);
+    }
+  }, [answer]);
+
+  return(
+    <>
+
+      <div className={style.words}>
+        {words && words.map((word, index) => (
+          <button className={`${style.word} 
+            ${(word.id !== answer?.id) && isAnswered ? style.wrongAnswers : ''}
+            ${(word.id === answer?.id) && isAnswered ? style.correctAnswer : ''}
+            ${(word.id === selected) && (word.id !== answer?.id) && isAnswered ? style.wrongAnswer : ''}
+            `} 
+
+            key={word.id} value={word.id} onClick={checkAnswer}>
+              <span className={style.wordIndex}>{index + 1}</span>
+              {word.wordTranslate}
+
+          </button>
+        
+        ))}
+      </div>
+       
+    </>
+  )
+}
