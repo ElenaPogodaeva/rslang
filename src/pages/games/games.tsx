0962983caf1f6e31535a59af15504037c024b5ed
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2022-02-22 05:19:45 +0300

    feat: add game page

--- src/pages/games/games.tsx
+++ src/pages/games/games.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from './games.scss';
 
 const GamesPage = () => {
   return (
-    <h2>Games</h2>
+    <div className={style.gameMenu}>
+      <div className={style.gameWrapper}>
+        <div className={style.cards}>
+        <div className={`${style.card} ${style.cardSprint}`}>
+            <Link className={style.cardLink} to="/games/sprint"></Link>
+            <h3 className={style.cardTitle}>Спринт</h3>
+          </div>
+    
+          <div className={`${style.card} ${style.cardAudiocall}`}>
+            <Link className={style.cardLink} to="/games/audiocall"></Link>
+            <h3 className={style.cardTitle}>Аудиовызов</h3>
+          </div>
+  
+          <div className={`${style.card} ${style.cardSavanna}`}>
+            <Link className={style.cardLink} to="/games/savanna"></Link>
+            <h3 className={style.cardTitle}>Саванна</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
